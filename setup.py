f3ffb6a37277324b90ab09896b1618eff01c4b76
Niklas Böhm <mail@jnboehm.com>
2024-12-19 03:18:54 +0100

    Fix setup.py compilation
    Change file to .c so that it's actually found within the source distribution.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 
 package = Extension(
     "bh_tforce",
-    ["src/tfdp/bh_tforce/bh_tforce.pyx"],
+    ["src/tfdp/bh_tforce/bh_tforce.c"],
     include_dirs=[numpy.get_include()],
     extra_compile_args=["-fopenmp"],
     extra_link_args=["-fopenmp"],
